239f88c2f8f7851fbedf5e3b33c4926ad1673d1a
sterbb <webjrdivinagracia@gmail.com>
2023-02-20 23:59:35 +0800

    version 2 - individual employee biometrics

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function(){
 
 
+
     // const input1 = $("#bio_data")[0].files[0];
     // input1.on("change", function(e){
     //     console.log(input1);
@@ -37,12 +38,13 @@ $(function(){
                 var d_t = number[1];
                 var id = number[0];
 
-                dt = d_t.split(" ").map(function(line){
-                    return line.split(" ");
-                });
+                var dt = new Date(d_t);
 
+                //var timedate = date.getTime;
 
-                records.push({"id":id,"date":dt[0][0],"time":dt[1][0] });
+                console.log(dt);
+
+                records.push({"id":id,"date_time":dt});
 
                 
             });
@@ -73,31 +75,136 @@ $(function(){
             
             console.log(emp_at[0]);
 
+            var employee_b = [];
             emp_at.forEach(emp_num =>{
-
-
+                var biometrics = [];
+                var morning = [];
+                var afternoon = [];
+    
                 records.forEach(number =>{
                     if(emp_num == number["id"]){
-                        //var time = new Date(number["time"]).toLocaleTimeString("en-US");
-                        if(number["time"] < 12){
-                            console.log(number["time"] + "morning");
+                        var time = number["date_time"];
+                        if(time.getHours() < 13){
+                            console.log(emp_num + time + "morning");
+                            morning.push(time);
                         }else{
-                            console.log(number["time"] + "afternoon");
+                            console.log(emp_num+ time + "afternoon");
+                            afternoon.push(time);
                         }
                         
                     }
+                   
                 });
-                                
+                biometrics.push(emp_num);
+                biometrics.push(morning);
+                biometrics.push(afternoon);
 
-
+                employee_b.push(biometrics);
             })
+
+            console.log(employee_b);
+
+            var femployee_b = [];
+
+            employee_b.forEach(number=>{
+            
+                var biometrics =[];
+                biometrics.push(number[0]);
+                biometrics.push("January 13, 2023");
+                
+                //morning time in and time out
+                for(let i =0; i < number[1].length; i++){
+                    var d_t = number[1][i];
+
+                    //time in
+                    if(d_t.getHours() <= 8 && d_t.getMinutes() <= 45){
+                        biometrics.push(number[1][i]);
+
+                        for(let i =0; i < number[1].length; i++){
+                            var d_t = number[1][i];
+        
+                            if(d_t.getHours() <= 12 && d_t.getMinutes() >= 30){
+                                biometrics.push(number[1][i]);
+                                break;
+                            }
+                        
+                        }
+                        
+                        break;
+                    }
+                }
+                //check morning absent (no time in/time out)
+                if(biometrics.length != 4){
+                    if(biometrics.length == 3){
+                        biometrics.push("ABSENT");
+                    }else{
+                        biometrics.push("ABSENT");
+                        biometrics.push("ABSENT");
+                    }
+                }
+
+                for(let i =0; i < number[2].length; i++){
+                    var d_t = number[2][i];
+
+                    //time in
+                    if(d_t.getHours() <= 13 && d_t.getMinutes() <= 45){
+                        biometrics.push(number[2][i]);
+
+                        for(let i =0; i < number[2].length; i++){
+                            var d_t = number[2][i];
+        
+                            if(d_t.getHours() >= 17 && d_t.getMinutes() <= 59){
+                                biometrics.push(number[2][i]);
+                                break;
+                            }
+                        
+                        }
+                        break;
+                    }
+                }
+
+                  //check if afternoon absent (no time in/time out)
+                  if(biometrics.length != 6){
+                    if(biometrics.length == 5){
+                        biometrics.push("ABSENT");
+                    }else{
+                        biometrics.push("ABSENT");
+                        biometrics.push("ABSENT");
+                    }
+                }
+
+
+                femployee_b.push(biometrics);
+        
+                console.log(biometrics);
+            });
+
+
+               console.log(femployee_b);
+        
+
+            femployee_b.forEach(employee=>{
+                var biometrics_table = $("#biometrics > tbody");
+                var data = '<tr  class="bg-gray-500 text-white">';
+                var morning;
+                var afternoon;
+                
+                data += '<td>'+employee[0] + '</td>'    
+                data += '<td>'+employee[1] + '</td>'
+                data += '<td>'+getTime(employee[2]) + '</td>'
+                data += '<td>'+getTime(employee[3]) + '</td>'
+                data += '<td>'+getTime(employee[4]) + '</td>'
+                data += '<td>'+getTime(employee[5]) + '</td>'
+                data+= '</tr>'
+
+
+                biometrics_table.append(data);
+            });
+
+            
 
 
         }
-
-    
-        
-
 
 
         // const reader = new FileReader();
@@ -115,3 +222,17 @@ $(function(){
     // })
 
 });
+
+
+function getTime(time){
+    if(time != "ABSENT"){
+        var date = new Date(time);
+        var hour =date.getHours();
+        var minutes =date.getMinutes();
+        var seconds =date.getSeconds();
+        var result = hour + ":" + minutes + ":" + seconds;
+        return result;
+    }else{
+        return time;
+    }
+}
